a8834bb9bce6872320acf37bfceb5135d9fd4a60
eigenemara <eigen.emara@gmail.com>
2022-10-19 15:19:19 +0200

    add usage message to unv-report

--- utilities/unv-report.cpp
+++ utilities/unv-report.cpp
@@ -8,6 +8,7 @@
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         std::cerr << "Too few args" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [input]" << std::endl;
         return -1;
     }
 
